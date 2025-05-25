787614448e162312fba51b543cecbbcec7efdbbc
virtualguard <virtualguard101@gmail.com>
2025-05-25 18:17:43 +0800

    update C++ function explain

--- src/three_body.h
+++ src/three_body.h
@@ -10,6 +10,10 @@ struct Body {
   Vec3 vel;
 };
 
-// 计算 N 体在 dt 时间后的一步新状态
+/**
+* 计算 N 体在 dt 时间后的一步新状态
+* @param bodies 使用结构体 Body 构造的天体实例参数。由 mass（质量）、pos（初始位置）、vec（初始速度）三个成员变量组成.
+* @param dt 时间步长.
+*/ 
 void step(std::vector<Body>& bodies, double dt);
 
